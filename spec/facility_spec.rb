6e8f1da6baf4e6a7d8c24f38f3fb84385578da00
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-07-16 20:44:43 -0600

    added registrant details to the facility tests

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -1,3 +1,6 @@
+require './lib/registrant'
+require './lib/facility'
+require './lib/vehicle'
 require 'spec_helper'
 
 RSpec.describe Facility do
@@ -21,6 +24,115 @@ RSpec.describe Facility do
       @facility.add_service('Renew Drivers License')
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+
+  describe '#vehicle registration' do
+    it 'can register vehicles' do
+
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      facility_1.add_service('Vehicle Registration')
+      cruz.registration_date
+      facility_1.registered_vehicles
+      facility_1.collected_fees
+      facility_1.register_vehicle(cruz)
+      cruz.registration_date
+      cruz.plate_type
+      facility_1.registered_vehicles
+      facility_1.collected_fees
+
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      facility_1.register_vehicle(bolt)
+      bolt.registration_date
+      bolt.plate_type
+      facility_1.registered_vehicles
+      facility_1.collected_fees
+      facility_2.registered_vehicles
+      facility_2.services
+      facility_2.register_vehicle(bolt)
+      facility_2.registered_vehicles
+      facility_2.collected_fees
+
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+      facility_1.register_vehicle(camaro)
+      camaro.registration_date
+      camaro.plate_type
+
+    end
+  end
+
+  describe '#administer written tests' do
+    it 'can administer written tests' do
+
+      registrant_1 = Registrant.new('Bruce', 18, true)
+      registrant_2 = Registrant.new('Penny', 16, false)
+      registrant_3 = Registrant.new('Tucker', 15, false)
+
+      registrant_1.license_data
+      registrant_1.permit?
+      facility_1.administer_written_test(registrant_1)
+      registrant_1.license_data
+      facility_1.add_service('Written Test')
+      facility_1.administer_written_test(registrant_1)
+      registrant_1.license_data
+
+      registrant_2.age
+      registrant_2.permit?
+      facility_1.administer_written_test(registrant_2)
+      registrant_2.earn_permit
+      facility_1.administer_written_test(registrant_2)
+      registrant_2.license_data
+
+      registrant_3.age
+      registrant_3.permit?
+      facility_1.administer_written_test(registrant_3)
+      registrant_3.earn_permit
+      facility_1.administer_written_test(registrant_3)
+      registrant_3.license_data
+
+    end
+  end
+
+  describe '#administers road tests' do
+    it 'can administer road tests' do
+
+      registrant_1 = Registrant.new('Bruce', 18, true)
+      registrant_2 = Registrant.new('Penny', 16, false)
+      registrant_3 = Registrant.new('Tucker', 15, false)
+
+      facility_1.administer_road_test(registrant_3)
+      registrant_3.earn_permit
+      facility_1.administer_road_test(registrant_3)
+      registrant_3.license_data
+
+      facility_1.administer_road_test(registrant_1)
+      facility_1.add_service('Road Test')
+      facility_1.administer_road_test(registrant_1)
+      registrant_1.license_data
+
+      facility_1.administer_road_test(registrant_2)
+      registrant_2.license_data
+
+    end
+  end
+
+  describe '#renew drivers licenses' do
+    it 'it can renew drivers licenses' do
+
+      registrant_1 = Registrant.new('Bruce', 18, true)
+      registrant_2 = Registrant.new('Penny', 16, false)
+      registrant_3 = Registrant.new('Tucker', 15, false)
+
+      facility_1.renew_drivers_license(registrant_1)
+      facility_1.add_service('Renew License')
+      facility_1.renew_drivers_license(registrant_1)
+      facility_1.renew_drivers_license(registrant_1)
+
+      facility_1.renew_drivers_license(registrant_3)
+      registrant_3.license_data
+
+      facility_1.renew_drivers_license(registrant_2)
+      registrant_2.license_data
     end
   end
 end
